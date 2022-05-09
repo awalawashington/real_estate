7ffa3c9500e7c19243fa1f4c93d957ed4124705f
Washington Awala <washingtonawala@washingtonawala@webmasters.co.ke>
2022-05-09 20:49:41 +0300

    First Commit

---
+++ app/Models/PropertyTranslation.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PropertyTranslation extends Model
+{
+    protected $fillable = [       
+        'building_age',
+        'parking',
+        'cooling',
+        'heating',
+        'sewer',
+        'water',
+        'torage_room',
+        'exercise_room',
+        'description',
+    ];
+
+    public $timestamps =false;
+}
